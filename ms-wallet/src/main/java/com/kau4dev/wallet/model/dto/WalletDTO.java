b6cf5cb72abb407182eb8bdb1786c8fef344bc12
kau4dev <kauavictordev7@gmail.com>
2026-01-02 11:54:57 -0300

    feat: adiciona entidade Wallet e DTO, configurações de banco de dados e dependências necessárias

---
+++ ms-wallet/src/main/java/com/kau4dev/wallet/model/dto/WalletDTO.java
@@ -0,0 +1,20 @@
+package com.kau4dev.wallet.model.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record WalletDTO (
+        UUID id,
+
+        @NotNull(message = "Balance is required")
+        BigDecimal balance,
+
+        @NotNull(message = "Version is required")
+        Long version,
+
+        @NotNull(message = "Id User is required")
+        UUID idUser
+) {
+}
---
+++ ms-wallet/src/main/java/com/kau4dev/wallet/model/entity/Wallet.java
@@ -0,0 +1,33 @@
+package com.kau4dev.wallet.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "wallets")
+@Table(name = "tb_wallets")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private BigDecimal balance = BigDecimal.ZERO;
+ 
+    @Version
+    @Column(nullable = false)
+    private Long version = 0L;
+
+    @Column(name = "id_user", nullable = false)
+    private UUID idUser;
+
+
+}
